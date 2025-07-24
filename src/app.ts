130e1101fb79a7345db5e942cd77e9f0a16dab4e
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-07-24 18:02:49 +0200

    adde husky , changed eslint.config and added cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
+app.use(cors({ origin: [process.env.LOCAL_HOST] as string[] }));
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
 app.use(
-  (
-    err: { status: number; message: string },
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
+  (err: { status: number; message: string }, req: Request, res: Response) => {
     const { status = 500, message = "Server error" } = err;
     res.status(status).json({ message });
   }
