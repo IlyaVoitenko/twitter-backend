68c6a1a7e89623a5849a6bc7173fd1e00aee6462
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-08-03 13:48:14 +0200

    added tweetSchema

--- src/models/tweet.ts
+++ src/models/tweet.ts
@@ -1,7 +1,79 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model, Document } from "mongoose";
 
-export const tweetSchema = new Schema(
-  {},
+export interface ITweet extends Document {
+  content: string;
+  author: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+  likes: mongoose.Types.ObjectId[];
+  replies: mongoose.Types.ObjectId[];
+  retweetFrom?: mongoose.Types.ObjectId;
+  images?: { url: string; public_id?: string }[];
+  isDeleted: boolean;
+  views: number;
+  isPinned: boolean;
+  hashtags: string[];
+  mentions: mongoose.Types.ObjectId[];
+}
+export const tweetSchema = new Schema<ITweet>(
+  {
+    content: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    replies: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Tweet",
+      },
+    ],
+    retweetFrom: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+      default: null,
+    },
+    images: [
+      {
+        url: { type: String, required: true },
+        public_id: String,
+      },
+    ],
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+    hashtags: [
+      {
+        type: String,
+      },
+    ],
+    mentions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
   { versionKey: false, timestamps: true }
 );
 
